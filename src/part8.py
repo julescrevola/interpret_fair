46fb3b1b58b0a2bf184c0453d8dd725a052b474e
Jules Crevola <julescrevola@gmail.com>
2024-09-26 12:26:55 +0200

    part 8 updates

--- src/part8.py
+++ src/part8.py
@@ -7,40 +7,7 @@ from XPER.viz.Visualisation import visualizationClass as viz
 import joblib
 
 
-"""
-#load the dataset
-dataset = "our dataset"
-
-#load the model
-model = "our model"
-
-# Define the evaluation metric(s) to be used
-eval_metric = "AUC"
-
-# Evaluate the model performance using the specified metric(s)
-XPER_ = ModelPerformance(X_train.values, y_train.values, X_test.values, y_test.values, model)
-PM = XPER_.evaluate([eval_metric])
-
-# Print the performance metrics
-print("Performance Metrics: ", round(PM, 3))
-
-# Option 1 - Kernel True (more than 10 variables)
-# Calculate XPER values for the model's performance
-XPER_values = XPER_.calculate_XPER_values([eval_metric])
-
-labels = list(our_dataset.drop(columns='targetCol').columns)
-
-#bar plot
-viz.bar_plot(XPER_values=XPER_values, X_test=pd.DataFrame(X_test), labels=labels, p=6,percentage=True)
-
-#beeswarn plot
-viz.beeswarn_plot(XPER_values=XPER_values,X_test=pd.DataFrame(X_test),labels=labels)
-
-#force plot
-viz.force_plot(XPER_values=XPER_values, instance=1, X_test=X_test, variable_name=labels, figsize=(16,4))
-"""
-
-def xper_method(model, eval_metric, dataset: pd.DataFrame, X_train: pd.DataFrame, y_train: pd.Series, X_test: pd.DataFrame, y_test: pd.Series):
+def xper_method(model, eval_metric, dataset: pd.DataFrame, target_col: pd.Series, X_train: pd.DataFrame, y_train: pd.Series, X_test: pd.DataFrame, y_test: pd.Series):
     
     """
     Use XPER method to compute model performance and visualize outcome
@@ -57,13 +24,13 @@ def xper_method(model, eval_metric, dataset: pd.DataFrame, X_train: pd.DataFrame
     # Calculate XPER values for the model's performance
     XPER_values = XPER_.calculate_XPER_values([eval_metric], execution_type="ProcessPoolExecutor")
 
-    labels = list(X_train.columns)
+    labels = list(dataset.drop(target_col, axis=1).columns)
 
     #bar plot
-    viz.bar_plot(XPER_values=XPER_values, X_test=X_test, labels=labels, p=6,percentage=True)
+    viz.bar_plot(XPER_values=XPER_values, X_test=pd.DataFrame(X_test), labels=labels, p=6, percentage=True)
 
     #beeswarn plot
-    viz.beeswarn_plot(XPER_values=XPER_values,X_test=X_test, labels=labels)
+    viz.beeswarn_plot(XPER_values=XPER_values, X_test=pd.DataFrame(X_test), labels=labels)
 
     #force plot
     viz.force_plot(XPER_values=XPER_values, instance=1, X_test=X_test, variable_name=labels, figsize=(16,4))
