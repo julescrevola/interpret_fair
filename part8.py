b02cc41197aded22d10243f966b3d96fb3a98946
Giacomo <giacomo.lustri@hec.edu>
2024-09-26 18:00:22 +0200

    Added permutation importance

--- part8.py
+++ part8.py
@@ -4,41 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from XPER.compute.Performance import ModelPerformance
 from XPER.viz.Visualisation import visualizationClass as viz 
-import joblib
-
-
-"""
-#load the dataset
-dataset = "our dataset"
-
-#load the model
-model = "our model"
-
-# Define the evaluation metric(s) to be used
-eval_metric = "AUC"
-
-# Evaluate the model performance using the specified metric(s)
-XPER_ = ModelPerformance(X_train.values, y_train.values, X_test.values, y_test.values, model)
-PM = XPER_.evaluate([eval_metric])
-
-# Print the performance metrics
-print("Performance Metrics: ", round(PM, 3))
-
-# Option 1 - Kernel True (more than 10 variables)
-# Calculate XPER values for the model's performance
-XPER_values = XPER_.calculate_XPER_values([eval_metric])
-
-labels = list(our_dataset.drop(columns='targetCol').columns)
-
-#bar plot
-viz.bar_plot(XPER_values=XPER_values, X_test=pd.DataFrame(X_test), labels=labels, p=6,percentage=True)
-
-#beeswarn plot
-viz.beeswarn_plot(XPER_values=XPER_values,X_test=pd.DataFrame(X_test),labels=labels)
-
-#force plot
-viz.force_plot(XPER_values=XPER_values, instance=1, X_test=X_test, variable_name=labels, figsize=(16,4))
-"""
+from sklearn.inspection import permutation_importance
 
 def xper_method(model, eval_metric, dataset: pd.DataFrame, X_train: pd.DataFrame, y_train: pd.Series, X_test: pd.DataFrame, y_test: pd.Series):
     
@@ -68,7 +34,19 @@ def xper_method(model, eval_metric, dataset: pd.DataFrame, X_train: pd.DataFrame
     #force plot
     viz.force_plot(XPER_values=XPER_values, instance=1, X_test=X_test, variable_name=labels, figsize=(16,4))
 
-# Load the model
-model = joblib.load('cat_boost_model')
 
 
+#Permutation importance method
+from sklearn.inspection import permutation_importance
+scoring = ['r2']
+r_multi = permutation_importance(
+    model, X_train, y_train, n_repeats=30, random_state=0, scoring=scoring)
+
+for metric in r_multi:
+    print(f"{metric}")
+    r = r_multi[metric]
+    for i in r.importances_mean.argsort()[::-1]:
+        if r.importances_mean[i] - 2 * r.importances_std[i] > 0:
+            print(f"    {X_train.columns[i]:<8}"
+                  f"{r.importances_mean[i]:.3f}"
+                  f" +/- {r.importances_std[i]:.3f}")
